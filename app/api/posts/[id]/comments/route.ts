e7b6c237ec776709bb302fc82744d148c6023854
HAMZA-AOUN <aou4925@gmail.com>
2024-12-02 12:12:29 +0300

    resolve the type of id

--- app/api/posts/[id]/comments/route.ts
+++ app/api/posts/[id]/comments/route.ts
@@ -2,12 +2,18 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const { id } = params;
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${id}/comments`
   );
+  if (!res.ok) {
+    return NextResponse.json(
+      { error: "Failed to fetch comments" },
+      { status: res.status }
+    );
+  }
   const data = await res.json();
   return NextResponse.json(data);
 }
