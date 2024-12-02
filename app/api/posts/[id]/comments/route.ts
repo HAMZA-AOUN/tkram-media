371a0fb8eec5f64728a8e9dd11b79874defbf8d0
HAMZA-AOUN <aou4925@gmail.com>
2024-12-02 12:05:28 +0300

    resolve the type of id

--- app/api/posts/[id]/comments/route.ts
+++ app/api/posts/[id]/comments/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: number } }
 ) {
   const { id } = params;
   const res = await fetch(
